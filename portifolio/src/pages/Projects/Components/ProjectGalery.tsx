02dc2ac85d414ae645c8d6077df6b74230855e5d
chicogborba <chicogborba@gmail.com>
2023-03-28 12:45:59 -0300

    Adding deco to galery text

--- portifolio/src/pages/Projects/Components/ProjectGalery.tsx
+++ portifolio/src/pages/Projects/Components/ProjectGalery.tsx
@@ -6,12 +6,18 @@ const ProjectGalery = () => {
   return (
     <>
       <div className="bg-[#FFA722] w-full h-auto py-8">
-        <TitleOutlined
-          className="mb-4"
-          text={"Galery"}
-          color="pink"
-          align="center"
-        />
+        <div className="flex w-full h-auto justify-center items-center">
+          <div className="w-60 h-0 border-2 border-black rounded-full" />
+          <div className="font-Special-Elite text-5xl -mb-4 mx-4">/</div>
+          <TitleOutlined
+            className="mb-4"
+            text={"Galery"}
+            color="pink"
+            align="center"
+          />
+          <div className="font-Special-Elite text-5xl -mb-4 mx-4">/</div>
+          <div className="w-60 h-0 border-2 border-black rounded-full" />
+        </div>
         <PhotoSlider />
       </div>
     </>
